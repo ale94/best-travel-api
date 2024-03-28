e1905c7ee8beac2af8e2a85081273bdfce06c20d
ale94 <alejandrorua111@gmail.com>
2024-03-28 12:27:11 -0300

    Add docker compose, esquema y data sql

---
+++ src/main/java/ar/com/alejandro/best_travel_api/BestTravelApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.alejandro.best_travel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTravelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/alejandro/best_travel_api/BestTravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alejandro.best_travel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
