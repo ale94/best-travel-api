4588173e836a45d8898238bbd010869c6cfe1d6a
ale94 <alejandrorua111@gmail.com>
2024-03-31 11:21:35 -0300

    Add TicketController metodo GET

--- src/main/java/ar/com/alejandro/best_travel_api/api/controllers/TicketController.java
+++ src/main/java/ar/com/alejandro/best_travel_api/api/controllers/TicketController.java
@@ -5,13 +5,12 @@ import ar.com.alejandro.best_travel_api.api.models.responses.TicketResponse;
 import ar.com.alejandro.best_travel_api.infraestructure.abstract_services.ITicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("ticket")
+@RequestMapping(path = "ticket")
 @RequiredArgsConstructor
 public class TicketController {
 
@@ -20,6 +19,11 @@ public class TicketController {
     @PostMapping
     public ResponseEntity<TicketResponse> post(@RequestBody TicketRequest request) {
         return ResponseEntity.ok(ticketService.create(request));
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<TicketResponse> get(@PathVariable UUID id) {
+        return ResponseEntity.ok(ticketService.read(id));
     }
 
 
--- src/main/java/ar/com/alejandro/best_travel_api/infraestructure/services/TicketService.java
+++ src/main/java/ar/com/alejandro/best_travel_api/infraestructure/services/TicketService.java
@@ -48,7 +48,8 @@ public class TicketService implements ITicketService {
 
     @Override
     public TicketResponse read(UUID id) {
-        return null;
+        var ticketFromDB = ticketRepository.findById(id).orElseThrow();
+        return this.entityToResponse(ticketFromDB);
     }
 
     @Override
