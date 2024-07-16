f9d3f8f1b60defd4db23bd1f27b2f9a4d57f2507
ale94 <alejandrorua111@gmail.com>
2024-07-15 22:35:30 -0300

    feat: add TourController

--- src/main/java/ar/com/alejandro/best_travel_api/api/controllers/TourController.java
+++ src/main/java/ar/com/alejandro/best_travel_api/api/controllers/TourController.java
@@ -1,2 +1,33 @@
-package ar.com.alejandro.best_travel_api.api.controllers;public class TourController {
+package ar.com.alejandro.best_travel_api.api.controllers;
+
+import ar.com.alejandro.best_travel_api.api.models.requests.TourRequest;
+import ar.com.alejandro.best_travel_api.api.models.responses.TourResponse;
+import ar.com.alejandro.best_travel_api.infraestructure.abstract_services.ITourService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "tour")
+@AllArgsConstructor
+public class TourController {
+
+    private final ITourService tourService;
+
+    @PostMapping
+    public ResponseEntity<TourResponse> post(@RequestBody TourRequest request) {
+        return ResponseEntity.ok(tourService.create(request));
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<TourResponse> get(@PathVariable Long id) {
+        return ResponseEntity.ok(tourService.read(id));
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.tourService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
