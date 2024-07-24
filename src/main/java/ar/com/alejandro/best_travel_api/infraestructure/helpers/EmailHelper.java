df74e2e10fcde35df4ae09b21c24706620d5115e
ale94 <alejandrorua111@gmail.com>
2024-07-24 20:17:07 -0300

    feat: add email_template.html

--- src/main/java/ar/com/alejandro/best_travel_api/infraestructure/helpers/EmailHelper.java
+++ src/main/java/ar/com/alejandro/best_travel_api/infraestructure/helpers/EmailHelper.java
@@ -8,6 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 @Component
 @AllArgsConstructor
 @Slf4j
@@ -17,7 +23,7 @@ public class EmailHelper {
 
     public void sendMail(String to, String name, String product) {
         MimeMessage message = mailSender.createMimeMessage();
-        String htmlContent = "";
+        String htmlContent = this.readHTMLTemplate(name, product);
 
         try {
             message.setFrom(new InternetAddress("alejandrorua111@gmail.com"));
@@ -27,6 +33,17 @@ public class EmailHelper {
         } catch (MessagingException e) {
             log.error("Error to send mail", e);
         }
-
     }
+
+    public String readHTMLTemplate(String name, String product) {
+        try (var lines = Files.lines(TEMPLATE_PATH)) {
+            var html = lines.collect(Collectors.joining());
+            return html.replace("{name}", name).replace("{product}", product);
+        } catch (IOException e) {
+            log.error("Cant read html template", e);
+            throw new RuntimeException();
+        }
+    }
+
+    private final Path TEMPLATE_PATH = Paths.get("src/main/resources/email/email_templates.html");
 }
